caa80f1c069d7f53c5f0a742ae939766611ccb93
malav-mehta <malav.mehta@hotmail.com>
2021-04-27 10:15:15 -0400

    first commit

---
+++ precache-manifest.50b5bd6ec3c7e98c28933d76a7e71396.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b38256383cde7cd5faf5f9ac045318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07bd8f6be8f0dadb10b2",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4163110ebee3fd2e8dc",
+    "url": "/static/css/main.8fcb0c9a.chunk.css"
+  },
+  {
+    "revision": "07bd8f6be8f0dadb10b2",
+    "url": "/static/js/2.b6797a7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b6797a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4163110ebee3fd2e8dc",
+    "url": "/static/js/main.03d7e1b4.chunk.js"
+  },
+  {
+    "revision": "4b036ce5e444165107be",
+    "url": "/static/js/runtime-main.0c9192d1.js"
+  },
+  {
+    "revision": "a5de5f5bab8f611e1dc88fa1e69e35b2",
+    "url": "/static/media/01.a5de5f5b.png"
+  },
+  {
+    "revision": "06ab3f40db60c9a580b6d32e31eb74e8",
+    "url": "/static/media/02.06ab3f40.png"
+  },
+  {
+    "revision": "1cebfd419d31f5cac833d235d526d651",
+    "url": "/static/media/03.1cebfd41.png"
+  },
+  {
+    "revision": "d9c14540d19ebea9b79fcc80f6911fc5",
+    "url": "/static/media/04.d9c14540.png"
+  },
+  {
+    "revision": "9da33cf448d08c5c835bd5cfb697f8cd",
+    "url": "/static/media/05.9da33cf4.png"
+  },
+  {
+    "revision": "553fdba9674d94a00be7ca1c46c02620",
+    "url": "/static/media/06.553fdba9.png"
+  },
+  {
+    "revision": "73ac8800291fbfe6d2f34a7a22a3030e",
+    "url": "/static/media/07.73ac8800.png"
+  },
+  {
+    "revision": "6d678190abf4b92daa16a7d01ad7884c",
+    "url": "/static/media/08.6d678190.png"
+  },
+  {
+    "revision": "b2788404ef0eee1c4b48fa4d96a5ddab",
+    "url": "/static/media/09.b2788404.png"
+  },
+  {
+    "revision": "25b8202fc063223edb1fefb02ab4ea65",
+    "url": "/static/media/10.25b8202f.png"
+  },
+  {
+    "revision": "0006095026ec44bf65dfa1437f03d8e9",
+    "url": "/static/media/11.00060950.png"
+  },
+  {
+    "revision": "01343bf0ef2156949b9b4bada5bf9829",
+    "url": "/static/media/12.01343bf0.png"
+  },
+  {
+    "revision": "8b5de4182e777b15e984e8f34f1f161a",
+    "url": "/static/media/13.8b5de418.png"
+  },
+  {
+    "revision": "1c637bd183e9b7183062e8eab61a4f0b",
+    "url": "/static/media/14.1c637bd1.png"
+  },
+  {
+    "revision": "cac49adc8b513969335ff3863d5d00ba",
+    "url": "/static/media/15.cac49adc.png"
+  },
+  {
+    "revision": "49be380b76f519441b1a578424fa76c4",
+    "url": "/static/media/16.49be380b.png"
+  },
+  {
+    "revision": "fa21c62fcbe9f904ba172771b401d632",
+    "url": "/static/media/17.fa21c62f.png"
+  },
+  {
+    "revision": "6cdbf30b1fe085e88127fb6776dd5c5c",
+    "url": "/static/media/1password.6cdbf30b.png"
+  },
+  {
+    "revision": "691c1039155db4af1fdbf6770d774eb9",
+    "url": "/static/media/balsamiq.691c1039.png"
+  },
+  {
+    "revision": "5c6aa279c14bd27ac3d3517ac97b6c82",
+    "url": "/static/media/bars.5c6aa279.svg"
+  },
+  {
+    "revision": "be6f6adf586acd9973feaeb370751896",
+    "url": "/static/media/bugsee.be6f6adf.png"
+  },
+  {
+    "revision": "f7a2118bb7db5d6fb3bb0cd0783988c6",
+    "url": "/static/media/digitalOcean.f7a2118b.png"
+  },
+  {
+    "revision": "e6617a4a4d2a6057b357b50250e91375",
+    "url": "/static/media/echoar.e6617a4a.png"
+  },
+  {
+    "revision": "9f56a2f3f710296d93961c600cced1d4",
+    "url": "/static/media/edgeprim.9f56a2f3.png"
+  },
+  {
+    "revision": "a264f71e3cb926a65746b158289102ac",
+    "url": "/static/media/linode.a264f71e.png"
+  },
+  {
+    "revision": "d8d391388d5d5dc5e49910f5a99ba959",
+    "url": "/static/media/logo-alt.d8d39138.png"
+  },
+  {
+    "revision": "71f206e315e460b88cdeb8ad80f9d998",
+    "url": "/static/media/logo.71f206e3.png"
+  },
+  {
+    "revision": "149e145995cb7d1b751c2e9c8ef021bd",
+    "url": "/static/media/replit.149e1459.png"
+  },
+  {
+    "revision": "c63fc6960574b321c01644a10e9bbf2a",
+    "url": "/static/media/tohacks.c63fc696.png"
+  },
+  {
+    "revision": "a47ff262c421cc8667b960b6dfd6043d",
+    "url": "/static/media/wolframlanguage.a47ff262.png"
+  }
+]);
